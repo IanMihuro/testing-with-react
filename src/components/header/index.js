1f57714e1c7661c55e3d18528fe8a505223ef8d3
IanMihuro <nickmihuro@gmail.com>
2019-10-02 13:51:59 +0300

    header test added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,10 +5,10 @@ import Logo from "../../assets/graphics/logo.png";
 
 export default function Header(props) {
   return (
-    <header>
+    <header data-test="headerComponent">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="logo" />
+          <img data-test="logoIMG" src={Logo} alt="logo" />
         </div>
       </div>
     </header>
